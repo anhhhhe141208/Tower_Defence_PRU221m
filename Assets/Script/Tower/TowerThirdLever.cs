9c9830b39a92d6b0e35a668446bf948e54770ae8
lamdkhe153553 <lamdkhe153553@fpt.edu.vn>
2023-03-13 13:23:03 +0700

    modify folder monster

--- Assets/Script/Tower/TowerThirdLever.cs
+++ Assets/Script/Tower/TowerThirdLever.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerThirdLever : MonoBehaviour, IShooter
-{
+{   
+    // xoay
+
+    // chon muc tieu
+
     public void Shoot()
     {
         throw new System.NotImplementedException();
