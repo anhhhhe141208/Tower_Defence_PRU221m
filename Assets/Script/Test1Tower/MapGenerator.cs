5cb68351ead59dc1db6c7a3ad92c97dd7b3a4559
phamthaihoang1234 <namtuocbongdem1234567890@gmail.com>
2023-03-14 10:34:11 +0700

    add design pattern

--- Assets/Script/Test1Tower/MapGenerator.cs
+++ Assets/Script/Test1Tower/MapGenerator.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -164,4 +164,3 @@ public class MapGenerator : MonoBehaviour
 
     }
 }
-*/
